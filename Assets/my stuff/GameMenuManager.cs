24db182a246a8e4c73668593a16bfd9c4b141cd8
smokingkrillz <89337823+smokingkrillz@users.noreply.github.com>
2026-04-15 14:56:17 +0200

    final touches

--- Assets/my stuff/GameMenuManager.cs
+++ Assets/my stuff/GameMenuManager.cs
@@ -1,9 +1,18 @@
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class GameMenuManager : MonoBehaviour
 {
     public Transform head;
+    public GameObject sliderObject1;
+    public GameObject sliderObject2;
+    public UnityEngine.UI.Slider slider1;
+    public UnityEngine.UI.Slider slider2;
+    public UnityEngine.UI.Toggle toggle;
     public float spawnDistance = 2;
     public GameObject menu;
     public InputActionProperty showButton;
@@ -17,6 +26,7 @@ public class GameMenuManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
     if (showButton.action.WasPressedThisFrame())
     {
       menu.SetActive(!menu.activeSelf);
@@ -26,5 +36,23 @@ public class GameMenuManager : MonoBehaviour
     menu.transform.LookAt(new Vector3(head.position.x, menu.transform.position.y, head.position.z));
 
     menu.transform.forward *= -1;
+
+    
+
+    if (toggle.isOn)
+    {
+    sliderObject2.SetActive(true);
+    sliderObject1.SetActive(false);
+    slider1.value = 0;
+    //slider2.value = 0;
     }
+    if (!toggle.isOn)
+    {
+    sliderObject2.SetActive(false);
+    sliderObject1.SetActive(true);
+    //slider1.value = -20;
+    slider2.value = 0;
+    }
+    }
+    
 }
--- Assets/my stuff/water/TransformWater.cs
+++ Assets/my stuff/water/TransformWater.cs
@@ -7,7 +7,6 @@ public class TransformWater : MonoBehaviour
     public Slider age;
     public Slider age2;
     private float ageNumber;
-    private float ageNumber2;
     public int version;
     void Start()
     {
@@ -20,18 +19,18 @@ public class TransformWater : MonoBehaviour
     {
         ageNumber = age.value;
         if (version == 1) { //water 
-            ageNumber = (age.value/20.0f)*2 - (age2.value / 20.0f) *2;
+            ageNumber = (age.value/20.0f)*2 + 2.0f - (age2.value / 320.0f) *2;
             this.transform.position = new Vector3(150.0f, ageNumber, 100.0f);
         }
         if (version == 2) { //big iceberg 
-            ageNumber = age.value - (age2.value * 0.02f);
+            ageNumber = age.value - (age2.value * 0.015f);
             if (ageNumber > 0) {ageNumber = 0;}
             this.transform.position = new Vector3(62.36708f, ageNumber, 141.2893f);
         }
         //when -2, get -14 (previous: when -2.5 get -12?)
         //when 0, do 2
         if (version == 3) { //small iceberg
-            ageNumber = ageNumber + 12.0f - (age2.value); //when -2, get -2, when 12 get 0 
+            ageNumber = ageNumber + 9.5f - (age2.value); //when -2, get -2, when 12 get 0 
             if (ageNumber > -2) {ageNumber = ageNumber*0.142857f -1.71429f;} //0.142857x−1.71429
             this.transform.position = new Vector3(62.36708f, ageNumber, 141.2893f);
         }
@@ -41,8 +40,8 @@ public class TransformWater : MonoBehaviour
             this.transform.position = new Vector3(0.0f, ageNumber, 0.0f);
         }
         if (version == 5)  { //water 2
-            ageNumber = -10.79f + (age.value / 20.0f) * 2 - (age2.value / 20.0f) * 2; //-10.79
-            this.transform.position = new Vector3(0.0f, ageNumber, 0.0f);
+            ageNumber = -8.0f + (age.value / 20.0f) * 2 - (age2.value / 20.0f) * 2; //-10.79
+            this.transform.position = new Vector3(23.17027f, ageNumber-1.0f, -14.86819f);
         }
     }   //-ageNumber2/40.0f + 5.0f //-10.79
 }
